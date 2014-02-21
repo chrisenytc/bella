5f924cf1e7b6176bc04c17e3161aaf1e8546bcd5
Christopher EnyTC <chris.enytc@gmail.com>
2014-02-21 18:05:16 -0300

    Upload v0.4.0

--- docs/api.js
+++ docs/api.js
@@ -1,7 +1,8 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "Bella"
+        "Bella",
+        "Generator"
     ],
     "modules": [],
     "allModules": []
